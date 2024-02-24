7116bb4269c0c6ffa49174a4f3053d2753db87ee
RezedaT <152332460+RezedaT@users.noreply.github.com>
2024-02-24 22:29:19 +0300

    Update Bank.java Fix #18

--- src/main/java/ru/netology/javaqadiplom/Bank.java
+++ src/main/java/ru/netology/javaqadiplom/Bank.java
@@ -1,32 +1,18 @@
-package ru.netology.javaqadiplom;
-
-public class Bank {
-
-    /**
-     * Операция перевода указанной суммы с одного счёта на другой.
-     * Если операция прошла успешно, то баланс счёта from должен
-     * уменьшиться на эту сумму, а баланс счёта to увеличиться.
-     * Если операция прошла неуспешно, балансы обоих счетов никак
-     * измениться не должны.
-     * @param from - счёт с которого переводим
-     * @param to - счёт на который переводим
-     * @param amount - сумма перевода
-     * @return - true если операция прошла успешно, false иначе
-     */
-    public boolean transfer(Account from, Account to, int amount) {
+ public boolean transfer(Account from, Account to, int amount) {
         if (amount <= 0) {
             return false;
         }
-        if (from.pay(amount)) {
-            to.add(amount);
-        }
- //           if (from.pay(amount) == false) {
- //           return false;
- //       }
-        if (to.add(amount) == false) {
-            from.balance += amount;
+        if (from.getBalance() < amount) { // Проверяем, достаточно ли средств на счете from
             return false;
         }
-        return true;
+        if (from.pay(amount)) {
+            if (to.add(amount)) {
+                return true; // Перевод прошел успешно
+            } else {
+                // Откатываем операцию списания средств с счета from
+                from.add(amount);
+                return false; // Перевод не удался из-за неудачного пополнения счета to
+            }
+        }
+        return false;
     }
-}
